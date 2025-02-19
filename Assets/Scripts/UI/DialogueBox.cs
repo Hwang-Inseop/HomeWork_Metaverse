66b697a1fa101ee0dbc28a2b6acd9d1bfcdb9f5c
Hwang-Inseop <inseop.h02@gmail.com>
2025-02-19 17:12:46 +0900

    [Feat] NPC 말풍선 출력
    메인씬 NPC에 UI 붙여서 말풍선 출력

---
+++ Assets/Scripts/UI/DialogueBox.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueBox : MonoBehaviour
+{
+    public float showTime = 3f;
+    public float hideTime = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    
+}
