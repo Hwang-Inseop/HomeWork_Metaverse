3b06c1cc0beefdf5d19108d5ca532a12e8da76f3
Hwang-Inseop <inseop.h02@gmail.com>
2025-02-19 09:29:53 +0900

    [chore] FollowCamera 스크립트 주석 첨부, 변수명 변경
    LastUpdate 메서드 내의 targetPosition 변수를 cameraPosition으로 이름 변경

--- Assets/Scripts/Entity/FollowCamera.cs
+++ Assets/Scripts/Entity/FollowCamera.cs
@@ -25,13 +25,14 @@ public class FollowCamera : MonoBehaviour
         if (target == null)
             return;
 
-        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+        //카메라 위치를 타겟=플레이어의 위치로 정의하되, z값은 원래 카메라의 위치로 정의
+        Vector3 cameraPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
 
         //카메라 이동 범위 제한
-        targetPosition.x = Mathf.Clamp(targetPosition.x, minBounds.x, maxBounds.x);
-        targetPosition.y = Mathf.Clamp(targetPosition.y, minBounds.y, maxBounds.y);
+        targetPosition.x = Mathf.Clamp(cameraPosition.x, minBounds.x, maxBounds.x);
+        targetPosition.y = Mathf.Clamp(cameraPosition.y, minBounds.y, maxBounds.y);
 
-        transform.position = targetPosition;
+        transform.position = cameraPosition;
     }
 
 }
