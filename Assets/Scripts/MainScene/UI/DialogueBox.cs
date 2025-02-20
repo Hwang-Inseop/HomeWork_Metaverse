6013d9d6c894d6465e9b7c8ccf786d2af2b6a8f2
Hwang-Inseop <inseop.h02@gmail.com>
2025-02-20 11:39:50 +0900

    [Feat] NPC 말풍선 시간차 출력 구현
    DialogueBox 스크립트 작성
    코루틴으로 반복 출력 구현
    Inspettor에서 지정한 시간만큼 말풍선을 출력, 숨김 반복

--- Assets/Scripts/MainScene/UI/DialogueBox.cs
+++ Assets/Scripts/MainScene/UI/DialogueBox.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class DialogueBox : MonoBehaviour
 {
-    public float showTime = 3f;
-    public float hideTime = 1f;
+    public GameObject dialogueBox; // 말풍선 오브젝트
+    public float showTime; // 말풍선 표시 시간
+    public float hideTime; // 사라진 후 대기 시간
 
     void Start()
     {
-        
+        // StartCoroutine() 일부 실행 → 잠시 멈춤 → 다시 실행을 할 수 있는 함수
+        StartCoroutine(ShowRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    // 코루틴을 만들려면 반환 타입을 IEnumerator로 만들어야 함
+    // yield return 키워드로 다음 코드 실행을 미루고 그 시점에서 기다림
+    IEnumerator ShowRoutine()
     {
+        while (true) // 무한 반복
+        {
+            dialogueBox.SetActive(true); // 말풍선 출력
+            yield return new WaitForSeconds(showTime); // showTime만큼 출력
 
+            dialogueBox.SetActive(false); // 말풍선 숨김
+            yield return new WaitForSeconds(hideTime); // hideTime만큼 숨김
+        }
     }
-    
-    
 }
